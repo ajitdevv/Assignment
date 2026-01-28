2ca21d1cd315df159a0103187ec98a2437534779
ajitdevv <ajitdev014@gmail.com>
2026-01-28 23:42:06 +0530

    .....

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -7,7 +7,7 @@ export function ProductProvider({ children }) {
   const [products, setProducts] = useState(initialProducts);
 
   return (
-    // <ProductContext.Provider value={{ products, setProducts }}>
+    <ProductContext.Provider value={{ products, setProducts }}>
       {children}
     </ProductContext.Provider>
   );
